f93ddd5238d05c5440c8b0d996d21e0e681b6730
ShammasAmir <amirshammas@gmail.com>
2023-07-10 08:50:55 +0330

    Create index view and list view.

---
+++ resources/views/admin/pages/coupon/index.blade.php
@@ -0,0 +1,45 @@
+@extends('admin.layout')
+
+@section('bread_crumb')
+    <li><a href="{{route('admin.coupon.index')}}">کوپن ها</a></li>
+    <li class="active"><a href="{{route('admin.coupon.index')}}">لیست کوپن ها</a></li>
+
+@endsection
+
+@section('main_content')
+    <div class="inner-container">
+        <div class="toolbar">
+            <ul>
+                @foreach(SortService::getSortableFields('coupon') as $sortable_field)
+                    <li class="btn btn-default {{$sortable_field->is_active ? "active" : ""}}"
+                        href="{{route('admin.null')}}?sort_model=coupon&sort_field={{$sortable_field->field}}&sort_method={{$sortable_field->method}}"
+                        act="link">
+                        @if($sortable_field->is_active)
+                            <i class="fa {{$sortable_field->method == SortMethod::ASCENDING ? "fa-long-arrow-up" : "fa-long-arrow-down"}}"></i>
+                        @endif
+                        {{$sortable_field->title}}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="inner-container has-toolbar has-pagination">
+            <div class="view-port">
+                @include('admin.pages.coupon.layout.list')
+            </div>
+            <div class="fab-container">
+                <div class="fab green">
+                    <button act="link" href="{{route('admin.coupon.create')}}">
+                        <i class="fa fa-plus"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        @include('admin.templates.pagination', [
+            "modelName" => "coupon",
+            "lastPage" => $colors->lastPage(),
+            "total" => $colors->total(),
+            "count" => $colors->perPage(),
+            "parentId" => $scope ?? null
+        ])
+    </div>
+@endsection
